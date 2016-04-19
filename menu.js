2219669176bcea22bafab28205944ebd734bd439
clairecofelice <clairecofelice@claires-mbp.dhcp.wustl.edu>
2016-04-18 19:13:02 -0500

    Lab

--- menu.js
+++ menu.js
@@ -4,12 +4,18 @@ var order = '';
 
 // build a function to ask questions
 
-function question() {
-  // your code goes here
+function question(itemOptions) {
+  var itemOrdered = prompt(itemOptions);
+  order += itemOrdered + " ";
 }
 
 // call the QUESTION function with the order options
-question();
+question("Choose one of the following items to customize today: burrito, bowl, taco, salad");
+question("What kind of protein would you like: steak, carnitas, chicken, barbacoa, sofritas?");
+question("Would you like rice, beans, or fajita veggies?");
+question("Would you like to top it off with fresh tomato salsa, tomatillo red-chili salsa, sour cream, tomatillo green-chili salsa, roasted chili-corn salsa, lettuce, guac, or cheese?");
+question("Would you like to add chips, salsa, or guacamole? If not, leave the field empty.");
+question("Would you like to order a drink? Your options are: bottled water, juice, soft drinks, beer, and margaritas");
 
 // output their order
 document.write("<p>You ordered: " + order + "<p>");
